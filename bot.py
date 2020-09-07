4049a0bba34355bb6c5100fc374d97464a8e1db9
LYS2 <47649690+kaice123@users.noreply.github.com>
2020-09-07 13:17:57 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,8 +25,9 @@ async def on_ready():
     await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game(name='놀쟈아'))
     #온라인 online 자리비움 idle 방해금지 do_not_disturb, dnd
     print(f'히힛~!')
-
-token = ("NzI4ODY3MDAzODk2NTYxNzM1.XwFGhQ.5keZrM8GbsuNZoHpKFRixVBsRMQ")
+    
+access_token = os.environ["BOT_TOKEN"]
+token = ("access_token")
 
 client.run(token, reconnect=True) #리스 귀여움 >.< #이제 그럼 된거네
 
